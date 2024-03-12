70731aac5de864ec28db70abbc4034d6448369fc
hello <fijinshifas1@gmail.com>
2024-03-12 11:02:52 +0530

    initial

---
+++ lib/features/details_adding/depostry/adding_repository.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Addingrepository{
+  final FirebaseFirestore _firestore;
+  Addingrepository({required FirebaseFirestore firestore}):_firestore=firestore;
+  CollectionReference get 
+}
